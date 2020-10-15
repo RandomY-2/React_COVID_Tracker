a4557089fbdb7d9ea3bbd406ebfe916426719a93
Jiahe Yan <yjh020711@ucla.edu>
2020-10-15 19:32:33 +0800

    Finished covid  tracker app

--- src/api/index.js
+++ src/api/index.js
@@ -2,13 +2,38 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+  let changeableUrl = `${url}/countries/US`;
+
+  if (country) {
+    changeableUrl = `${url}/countries/${country}`;
+  }
+
+  try {
+    const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
+
+    return { confirmed, recovered, deaths, lastUpdate };
+  } catch (error) {
+    return error;
+  }
+};
+
+export const fetchDailyData = async () => {
     try {
-        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
-        
-        return { confirmed, recovered, deaths, lastUpdate }
-        
+      const { data } = await axios.get('https://api.covidtracking.com/v1/us/daily.json');
+  
+      return data.reverse().map(({ positive, recovered, death, dateChecked: date }) => ({ confirmed: positive, recovered, deaths: death, date }));
     } catch (error) {
-        
+      return error;
     }
-}
+  };
+
+export const fetchCountries = async () => {
+  try {
+    const { data: { countries } } = await axios.get(`${url}/countries`);
+
+    return countries.map((country) => country.name);
+  } catch (error) {
+    return error;
+  }
+};
